89f13eed4a9812ae6846a7c07b10127236d562a4
Pedro Henrique <pedro.lg.cs@gmail.com>
2022-07-25 10:38:01 -0300

    feat: configure envoriment variables

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import type { NextPageWithLayout } from 'pages/_app';
+import type { NextPageWithLayout } from '@/pages/_app';
 
 const Index: NextPageWithLayout = () => <h1>Home</h1>;
 
